a2b329c964c2a8d979015f5a370d467f8d1f1851
Sajeda Akter <sajeda16akter@gmail.com>
2023-10-15 22:26:10 +0600

    icon-verified-bonus-complete

--- main.js
+++ main.js
@@ -22,66 +22,77 @@ const displayCategory = (categories) => {
 };
 
 // all categories show by id
-const showAllCategories = async (category_id) => {
+const showAllCategories = async (category_id,isShow) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/videos/category/${category_id}`
   );
   const data = await res.json();
-  displayAllCategories(data.data);
+  displayAllCategories(data.data,isShow);
 };
 
 // all categories show by id
-const displayAllCategories = (categories) => {
+const displayAllCategories = (categories,isShow) => {
+  console.log(isShow)
   const container = document.getElementById("all-category");
 
   // clear the categories show display innerHTMl
   container.innerHTML = "";
 
   // all the category show in the HTMl
-  categories.forEach((categoryAll) => {
-    if (isNaN) {
-      function convertMsTOTime(seconds) {
-        if (!seconds) {
-          return "";
-        } else {
-          const hours = Math.floor(seconds / 3600);
-          const minutes = Math.floor((seconds - hours * 3600) / 60);
-          const second = seconds - hours * 3600 - minutes * 60;
+  if(categories.length>0){
+    categories.forEach((categoryAll) => {
+    const verified= categoryAll.authors[0].verified 
 
-          return `${hours}hrs ${minutes} min ${second}sec`;
+      if (isNaN) {
+        function convertMsTOTime(seconds) {
+          if (!seconds) {
+            return "";
+          } else {
+            const hours = Math.floor(seconds / 3600);
+            const minutes = Math.floor((seconds - hours * 3600) / 60);
+            const second = seconds - hours * 3600 - minutes * 60;
+  
+            return `${hours}hrs ${minutes} min ${second}sec`;
+          }
         }
       }
-    }
-
-    const { thumbnail, others, authors, title } = categoryAll;
-
-    // create category card
-    const div = document.createElement("div");
-    div.classList = "card w-80 bg-base-100 shadow-xl h-3/4 relative";
-    div.innerHTML = `
-<figure class="w-full h-2/4"><img src="${thumbnail}? ${thumbnail}:'Image Not Found" class=" w-full" /></figure>
-                <div class=" flex ml-4 gap-4 mt-4">
-                <img src="${
-                  authors[0].profile_picture
-                }" class="h-12 w-12 rounded-full"/>
-               <div>
-               <p class="font-semibold text-xl">${title}</p>
-               <div>
-               <p>${authors[0].profile_name}</p>
-               <p class="text-white font-semibold absolute top-20 right-0">${convertMsTOTime(
-                 categoryAll.others.posted_date
-               )}</p>
-                </div>
-                <p>${others.views} views</p>
-            </div>
-               </div>   
-                
-               
-                </div>
-
-`;
-    container.appendChild(div);
-  });
+  
+      const { thumbnail, others, authors, title } = categoryAll;
+  
+      // create category card
+      const div = document.createElement("div");
+      div.classList = "card w-80 bg-base-100 shadow-xl h-3/4 ";
+      div.innerHTML = `
+  <figure class="w-full h-2/4 relative"><img src="${thumbnail}? ${thumbnail}:'Image Not Found" class=" w-full" /></figure>
+                  <div class=" flex ml-4 gap-4 mt-4">
+                  <img src="${
+                    authors[0].profile_picture
+                  }" class="h-12 w-12 rounded-full"/>
+                 <div>
+                 <p class="font-semibold text-xl">${title}</p>
+                 <div class="flex gap-2">
+                 <p>${authors[0].profile_name}</p>
+                 <p>${(verified===true)?`<i class="fa-solid fa-circle-check text-indigo-600 "></i>`:''}</p>
+                  </div>
+                  <p>${others.views} views</p>
+                  <p class="text-white font-semibold absolute top-20 right-3">${convertMsTOTime(
+                    categoryAll.others.posted_date
+                  )}</p>
+              </div>
+                 </div>   
+                  
+                 
+                  </div>
+  
+  `;
+      container.appendChild(div);
+    });
+  }
+  else{
+    const noData=document.getElementById('noFoundData');
+  noData.style.display='block'
+  }
+ 
 };
 
 const sortByView = async () => {
@@ -89,74 +100,75 @@ const sortByView = async () => {
     "https://openapi.programming-hero.com/api/videos/category/1000"
   );
   const data = await res.json();
- sortByViewData(data.data)
-  // (data.data.forEach(datas=>{
-  //   const views=parseFloat((datas.others.views).slice(0,3))
-  //   console.log(views.sort(function(a,b){
-  //     return b-a
-  //   }))
-  // }))}
+displayAllCategories(data.data)
+
+
 };
 
-const sortByViewData = (categories) => {
-
-  const container = document.getElementById("all-category");
-
-  // clear the categories show display innerHTMl
-  container.innerHTML = "";
-
-  // all the category show in the HTMl
-  categories.forEach((categoryAll) => {
 
 
-    // convert seconds to hours and minute
-    if (isNaN) {
-      function convertMsTOTime(seconds) {
-        if (!seconds) {
-          return "";
-        } else {
-          const hours = Math.floor(seconds / 3600);
-          const minutes = Math.floor((seconds - hours * 3600) / 60);
-          const second = seconds - hours * 3600 - minutes * 60;
+// const sortByViewDisplay = (categories) => {
 
-          return `${hours}hrs ${minutes} min ${second}sec`;
-        }
-      }
-    }
+//   const container = document.getElementById("all-category");
 
-    // display the data desecdening By views
-    const views=parseFloat(categoryAll.others.views);
-   console.log(views)
+//   // clear the categories show display innerHTMl
+//   container.innerHTML = "";
 
-    const { thumbnail, others, authors, title } = categoryAll;
+// //   // all the category show in the HTMl
+// //   categories.forEach((categoryAll) => {
 
-    // create category card
-    const div = document.createElement("div");
-    div.classList = "card w-80 bg-base-100 shadow-xl h-3/4 relative";
-    div.innerHTML = `
-<figure class="w-full h-2/4"><img src="${thumbnail}? ${thumbnail}:'Image Not Found" class=" w-full" /></figure>
-                <div class=" flex ml-4 gap-4 mt-4">
-                <img src="${
-                  authors[0].profile_picture
-                }" class="h-12 w-12 rounded-full"/>
-               <div>
-               <p class="font-semibold text-xl">${title}</p>
-               <div>
-               <p>${authors[0].profile_name}</p>
-               <p class="text-white font-semibold absolute top-20 right-0">${convertMsTOTime(
-                 categoryAll.others.posted_date
-               )}</p>
-                </div>
-                <p>${others.views} views</p>
-            </div>
-               </div>   
+
+// //     // convert seconds to hours and minute
+// //     if (isNaN) {
+// //       function convertMsTOTime(seconds) {
+// //         if (!seconds) {
+// //           return "";
+// //         } else {
+// //           const hours = Math.floor(seconds / 3600);
+// //           const minutes = Math.floor((seconds - hours * 3600) / 60);
+// //           const second = seconds - hours * 3600 - minutes * 60;
+
+// //           return `${hours}hrs ${minutes} min ${second}sec`;
+// //         }
+// //       }
+// //     }
+
+// //     // display the data desecdening By views
+// //     // const viewAll=parseFloat(categoryAll.others.views);
+// //     const viewAll=categoryAll.others
+  
+  
+
+// //     const { thumbnail, others, authors, title } = categoryAll;
+
+// //     // create category card
+// //     const div = document.createElement("div");
+// //     div.classList = "card w-80 bg-base-100 shadow-xl h-3/4 relative";
+// //     div.innerHTML = `
+// // <figure class="w-full h-2/4"><img src="${thumbnail}? ${thumbnail}:'Image Not Found" class=" w-full" /></figure>
+// //                 <div class=" flex ml-4 gap-4 mt-4">
+// //                 <img src="${
+// //                   authors[0].profile_picture
+// //                 }" class="h-12 w-12 rounded-full"/>
+// //                <div>
+// //                <p class="font-semibold text-xl">${title}</p>
+// //                <div>
+// //                <p>${authors[0].profile_name}</p>
+// //                <p class="text-white font-semibold absolute top-20 right-0">${convertMsTOTime(
+// //                  categoryAll.others.posted_date
+// //                )}</p>
+// //                 </div>
+// //                 <p>${others.views} views</p>
+// //             </div>
+// //                </div>   
                 
                
-                </div>
+// //                 </div>
 
-`;
-    container.appendChild(div);
-  });
-};
+// // `;
+// //     container.appendChild(div);
+// //   });
+// };
+
 
 loadCategoryData();
